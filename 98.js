874d553602d155b250b5349677fc80165f2c36e5
yahay <124700074+Asimiraq@users.noreply.github.com>
2025-02-05 19:00:56 +0300

    98.js

---
+++ 98.js
@@ -0,0 +1,28 @@
+$(` <center><div></div><center></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$('a.label.label-primary.fl > img.fl').replaceWith('<img src="sico/z1casj3f6210.png" class="fl" style="margin-Right: 2px; width: 30px;">'),$(` <center><marquee direction="right" width="99%" id="asim55" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم</marquee></font><center>
+`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$(`<div class="uzr fl corner borderg mm bid1cb1etoid10" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(&quot;pic/1cb1bqui110.jpg.jpg&quot;);" class="fl fitimg hand u-pic    ">
+      <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523688860481">2د</span>
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/z1c9h490at10.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(77, 75, 69); background-color: rgb(209, 204, 194);" class="corner nosel u-topic dots fl hand">　　سآ’إْرٍة　　</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(0, 0, 0);" class=" u-msg   break  fl">
+أرَاكَ عليَّ أقْسَى النَّاسِ قَلباً
+ولي حالٌ ترقُّ لهُ القلوبُ
+حَبيبٌ أنتَ قلْ لي أمْ عَدوٌّ
+ففعلكَ ليسَ يفعلهُ حبيبُ !&nbsp;</div>
+      </div>
+&nbsp;<br />
+&nbsp;</div>
+      </div>
+    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="sico/z1c7j29n8e10.gif" etclass="fr" style="width:100%;margin-top: 1px;">
+</div>
+<div id="asim10"><center><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
+<a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> Smoke <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @MBC04 </p> Asim <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @o7700000 </p> Dhargam <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @D1997IQ </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$(`
+<link rel="stylesheet" href=""><style>
+
+
+</style>`).insertBefore('body')
+
